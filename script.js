2d96b49b31877fa1734b4a76907b82c0580d1892
Manishgurve <manishgurve25@gmail.com>
2023-03-27 11:49:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const person = {
     };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
-
-
+// variable to store the referance of players 
+const team = players;
+// variable to store copy of players
+const team1 = [...players];
+// variable to copy of persons object
+const cap1 = {...persons};
 export {players,person,team,team1,cap1}
